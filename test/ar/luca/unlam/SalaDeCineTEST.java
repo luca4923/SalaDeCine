c5c1ea5362fc2a7a34f327cab8960aedec92ee3c
luca4923 <martinezluca446@gmail.com>
2025-09-22 12:34:10 -0300

    CreoTest

---
+++ test/ar/luca/unlam/SalaDeCineTEST.java
@@ -0,0 +1,14 @@
+package ar.luca.unlam;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SalaDeCineTEST {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
