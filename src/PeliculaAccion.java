7c0d08a12683bad28a2658d2270306fcbf596785
luca4923 <martinezluca446@gmail.com>
2025-09-22 12:19:39 -0300

    PeliculaDeAccionyComedia

--- src/PeliculaAccion.java
+++ src/PeliculaAccion.java
@@ -1,10 +1,26 @@
 
 public class PeliculaAccion extends Pelicula {
 
-	public PeliculaAccion(String string, int i, int j) {
-		// TODO Auto-generated constructor stub
+	String descripcion = "";
+
+	public PeliculaAccion(String titulo, int duracion, int edad_minima) {
+		super(titulo, duracion, edad_minima);
+		
 	}
 
-	
+	@Override
+	public String get_sinopsis() {
+		return "Una pareja que cree tener la vida bajo control descubre que convivir trae caos, enredos y visitas inesperadas, pero también muchas risas.";
+	}
+
+	@Override
+	public String get_descripcion() {
+		descripcion += "Titulo: " + titulo + '\n';
+		descripcion += "Duracion: " + duracion + '\n';
+		descripcion += "Edad minima: " + edad_minima + '\n';
+		descripcion += "Genero: Accion";
+
+		return descripcion;
+	}
 
 }
--- src/PeliculaComedia.java
+++ src/PeliculaComedia.java
@@ -1,13 +1,25 @@
 
 public class PeliculaComedia extends Pelicula {
 	
-	String titulo;
-	int duracion;
+	String descripcion;
 
-	public PeliculaComedia(String titulo, int i, int j) {
-		// TODO Auto-generated constructor stub
+	public PeliculaComedia(String titulo, int duracion, int edad_minima) {
+		super(titulo, duracion, edad_minima);
 	}
 
-	
+	@Override
+	public String get_sinopsis() {
+		return null;
+	}
+
+	@Override
+	public String get_descripcion() {
+		descripcion += "Titulo: " + titulo + '\n';
+		descripcion += "Duracion: " + duracion + '\n';
+		descripcion += "Edad minima: " + edad_minima + '\n';
+		descripcion += "Genero: Accion";
+
+		return descripcion;
+	}
 
 }
