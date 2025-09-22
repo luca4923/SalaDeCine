9c11d9b6b7619976970f68125e7e541d43c648ff
luca4923 <martinezluca446@gmail.com>
2025-09-22 12:15:34 -0300

    pelicula_de_drama

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,7 +1,7 @@
 
 public class Pelicula {
 
-	
+	int eso;
 	
 	
 	
--- src/PeliculaDrama.java
+++ src/PeliculaDrama.java
@@ -1,4 +1,8 @@
 
 public class PeliculaDrama extends Pelicula {
 
+	public PeliculaDrama(String string, int i, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
 }
